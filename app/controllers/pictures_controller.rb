ce8129412ff6b4bf99b08cdbf3cf6af2be9a458a
Carter Li <carter@swtchev.com>
2017-10-21 20:41:48 +0000

    Completed show action
    Class updates

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,39 @@
+class PicturesController < ApplicationController
+  def new_form
+    render("pic_templates/blank_form.html.erb")
+  end
+  
+  def create_row
+    render("pic_templates/added_row.html.erb")
+  end
+  
+  def index
+    render("pic_templates/list_of_all.html.erb")
+  end
+  
+  def show
+    # Parameters: {"the_id"=>"20"}
+    
+    visited_id = params["the_id"]
+    
+    p = Photo.find(visited_id)
+    
+    @the_caption = p.caption
+    
+    @the_image_url = p.source
+    
+    render("pic_templates/details.html.erb")
+  end
+  
+  def edit_form
+    render("pic_templates/prefilled_form.html.erb")
+  end
+  
+  def update_row
+    render("pic_templates/modified_row.html.erb")
+  end
+  
+  def destroy_row
+    render("pic_templates/removed_row.html.erb")
+  end
+end
